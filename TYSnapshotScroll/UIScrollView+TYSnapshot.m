3ef6a6940bada9faa11401f44238306de5bc65b1
quyiming <quyiming@bytedance.com>
2020-02-09 00:54:41 +0800

    fix: layout bug & text view bug
    Change-Id: Iac50a851bd071eb074ce0624144515d0f11810df

--- TYSnapshotScroll/UIScrollView+TYSnapshot.m
+++ TYSnapshotScroll/UIScrollView+TYSnapshot.m
@@ -35,85 +35,86 @@
     //延迟0.3秒，避免有时候渲染不出来的情况
     dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(300 * NSEC_PER_MSEC)), dispatch_get_main_queue(), ^{
         self.contentOffset = CGPointZero;
+        dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(300 * NSEC_PER_MSEC)), dispatch_get_main_queue(), ^{
 
+            UIGraphicsBeginImageContextWithOptions(self.bounds.size,NO,[UIScreen mainScreen].scale);
 
-        UIGraphicsBeginImageContextWithOptions(self.bounds.size,NO,[UIScreen mainScreen].scale);
+            CGContextRef context = UIGraphicsGetCurrentContext();
 
-        CGContextRef context = UIGraphicsGetCurrentContext();
+            [self.layer renderInContext:context];
 
-        [self.layer renderInContext:context];
+            //[self drawViewHierarchyInRect:self.bounds afterScreenUpdates:NO];
 
-        //[self drawViewHierarchyInRect:self.bounds afterScreenUpdates:NO];
+            snapshotImage = UIGraphicsGetImageFromCurrentImageContext();
 
-        snapshotImage = UIGraphicsGetImageFromCurrentImageContext();
+            UIGraphicsEndImageContext();
 
-        UIGraphicsEndImageContext();
+            self.layer.frame = oldFrame;
+            //还原
+            self.contentOffset = oldContentOffset;
 
-        self.layer.frame = oldFrame;
-        //还原
-        self.contentOffset = oldContentOffset;
-        
-        if (snapShotMaskView.layer){
-            [snapShotMaskView.layer removeFromSuperlayer];
-        }
-        
-        if (snapshotImage != nil) {
-            finishBlock(snapshotImage);
-        }
+            if (snapShotMaskView.layer){
+                [snapShotMaskView.layer removeFromSuperlayer];
+            }
+
+            if (snapshotImage != nil) {
+                finishBlock(snapshotImage);
+            }
+        });
     });
 }
-                   
+
 /*
-#pragma mark - 获取屏幕快照
-/// snapshotView:需要截取的view
-+(UIImage *)screenSnapshotWithSnapshotView:(UIView *)snapshotView
-{
-    return [self screenSnapshotWithSnapshotView:snapshotView snapshotSize:CGSizeZero];
-}
+ #pragma mark - 获取屏幕快照
+ /// snapshotView:需要截取的view
+ +(UIImage *)screenSnapshotWithSnapshotView:(UIView *)snapshotView
+ {
+ return [self screenSnapshotWithSnapshotView:snapshotView snapshotSize:CGSizeZero];
+ }
 
-/// snapshotView:需要截取的view,snapshotSize:需要截取的size
-+(UIImage *)screenSnapshotWithSnapshotView:(UIView *)snapshotView snapshotSize:(CGSize )snapshotSize
-{
-    UIImage *snapshotImg;
+ /// snapshotView:需要截取的view,snapshotSize:需要截取的size
+ +(UIImage *)screenSnapshotWithSnapshotView:(UIView *)snapshotView snapshotSize:(CGSize )snapshotSize
+ {
+ UIImage *snapshotImg;
 
-    @autoreleasepool{
-        if (snapshotSize.height == 0|| snapshotSize.width == 0) {//宽高为0的时候没有意义
-            snapshotSize = snapshotView.bounds.size;
-        }
-        
-        //创建
-        UIGraphicsBeginImageContextWithOptions(snapshotSize,NO,[UIScreen mainScreen].scale);
-        
-        CGContextRef context = UIGraphicsGetCurrentContext();
+ @autoreleasepool{
+ if (snapshotSize.height == 0|| snapshotSize.width == 0) {//宽高为0的时候没有意义
+ snapshotSize = snapshotView.bounds.size;
+ }
 
-        [snapshotView.layer renderInContext:context];
-//        [snapshotView drawViewHierarchyInRect:snapshotView.bounds afterScreenUpdates:NO];
-        
-        //获取图片
-        snapshotImg = UIGraphicsGetImageFromCurrentImageContext();
-        
-        //关闭
-        UIGraphicsEndImageContext();
-    }
-    return snapshotImg;
-}
-*/
+ //创建
+ UIGraphicsBeginImageContextWithOptions(snapshotSize,NO,[UIScreen mainScreen].scale);
+
+ CGContextRef context = UIGraphicsGetCurrentContext();
+
+ [snapshotView.layer renderInContext:context];
+ //        [snapshotView drawViewHierarchyInRect:snapshotView.bounds afterScreenUpdates:NO];
+
+ //获取图片
+ snapshotImg = UIGraphicsGetImageFromCurrentImageContext();
+
+ //关闭
+ UIGraphicsEndImageContext();
+ }
+ return snapshotImg;
+ }
+ */
 
 - (instancetype )subScrollViewTotalExtraHeight:(void(^)(CGFloat subScrollViewExtraHeight))finishBlock{
     __block CGFloat extraHeight = 0.0;
-      
-    [self.subviews enumerateObjectsUsingBlock:^(__kindof UIView * _Nonnull obj, NSUInteger idx, BOOL * _Nonnull stop) {
-      if ([obj isKindOfClass:[UIScrollView class]]){
-          UIScrollView *scrollView = (UIScrollView *)obj;
 
-          if (scrollView.contentSize.height > scrollView.frame.size.height) {
-              extraHeight = scrollView.contentSize.height - scrollView.frame.size.height;
-          }
-          
-          [scrollView subScrollViewTotalExtraHeight:^(CGFloat subScrollViewExtraHeight) {
-              extraHeight += subScrollViewExtraHeight;
-          }];
-      }
+    [self.subviews enumerateObjectsUsingBlock:^(__kindof UIView * _Nonnull obj, NSUInteger idx, BOOL * _Nonnull stop) {
+        if ([obj isKindOfClass:[UIScrollView class]]){
+            UIScrollView *scrollView = (UIScrollView *)obj;
+
+            if (scrollView.contentSize.height > scrollView.frame.size.height) {
+                extraHeight = scrollView.contentSize.height - scrollView.frame.size.height;
+            }
+
+            [scrollView subScrollViewTotalExtraHeight:^(CGFloat subScrollViewExtraHeight) {
+                extraHeight += subScrollViewExtraHeight;
+            }];
+        }
     }];
 
 
